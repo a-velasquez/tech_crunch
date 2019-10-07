2d12d14f4b61e9240a80303eacab74f9c4f499b1
Alex Velasquez <jtinnesco@gmail.com>
2019-10-07 05:36:00 +0000

    added dependencies to tech_crunch.rb

--- lib/tech_crunch.rb
+++ lib/tech_crunch.rb
@@ -1,4 +1,13 @@
-require "tech_crunch/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+require 'byebug'
+
+
+require_relative "./tech_sites/version"
+require_relative "./tech_sites/article"
+require_relative "./tech_sites/cli"
+require_relative "./tech_sites/scraper"
 
 module TechCrunch
   class Error < StandardError; end
