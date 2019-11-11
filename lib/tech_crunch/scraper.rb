764d9fcb700f0c17fa5961ca598da1684a4ddcbb
Alex Velasquez <jtinnesco@gmail.com>
2019-11-11 07:04:39 +0000

    changed name attr_accessor to title

--- lib/tech_crunch/scraper.rb
+++ lib/tech_crunch/scraper.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Scraper
 
   def self.read
@@ -10,14 +12,14 @@ class Scraper
     articles << self.scrape_tech_crunch
 
     articles
+    binding.pry
   end
 
   def self.scrape_tech_crunch
     doc = Nokogiri::HTML(open("https://techcrunch.com"))
-    byebug
 
     a = Article.new
-    a.name = doc.css("h2.post-block__title").text.strip.split("\t\t\t\n\t\t\n\t\t\t\n\t\t\t\t")
+    a.title = doc.css("h2.post-block__title").text.strip.split("\t\t\t\n\t\t\n\t\t\t\n\t\t\t\t")
     a.author = doc.css("div.post-block__meta div span").text.split("\t\t\t\n\t\t\t\n\t\t\t\t\t\n\t\t\t\t")
     a.url = doc.css("a").attribute("href").value
     a.preview = doc.css("div.post-block__content").text.split("\t\n\t\t")
