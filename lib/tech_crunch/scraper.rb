da4beaf6dfe766cbbcc291a02c057bb86b7bc020
Alex Velasquez <jtinnesco@gmail.com>
2019-11-14 07:36:48 +0000

    able to add author and date published to Article objects

--- lib/tech_crunch/scraper.rb
+++ lib/tech_crunch/scraper.rb
@@ -8,12 +8,12 @@ class Scraper
     headlines = Nokogiri::HTML(open(html))
     headlines.css('div.post-block').each do |headline|
       headline_details = {}
-      headline_details[:title] = headline.at_css('h2.post-block__title a').text.strip
+      headline_details[:title]  = headline.at_css('h2.post-block__title a').text.strip,
+      headline_details[:author] = headline.at_css('div.river-byline').text.strip
 
       articles << headline_details
     end
 
-    # a.title = doc.css("h2.post-block__title").text.strip.split("\t\t\t\n\t\t\n\t\t\t\n\t\t\t\t")
     # a.author = doc.css("div.post-block__meta div span").text.split("\t\t\t\n\t\t\t\n\t\t\t\t\t\n\t\t\t\t")
     # a.url = doc.css("a").attribute("href").value
     # a.preview = doc.css("div.post-block__content").text.split("\t\n\t\t")
