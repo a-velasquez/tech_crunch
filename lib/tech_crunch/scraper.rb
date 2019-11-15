48be7a49d077c3142d46534fbbdf824640a4664d
Alex Velasquez <jtinnesco@gmail.com>
2019-11-15 02:17:54 +0000

    scrape_headlines now correctly pulls href attibute

--- lib/tech_crunch/scraper.rb
+++ lib/tech_crunch/scraper.rb
@@ -2,16 +2,16 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_headlines(homepage_url)
+  def self.scrape_headlines(index_url)
     articles = []
-    html = open(homepage_url)
+    html = open(index_url)
     headlines = Nokogiri::HTML(html)
-    headlines.css('div.post-block').each do |headline|
+    headlines.css('.post-block').each do |headline|
       headline_details = {}
-      headline_details[:author]  = headline.at_css('div.river-byline').css('.river-byline__authors').text.strip
-      headline_details[:href]    = headline.at_css('h2.post-block__title a').attribute('href').value,
-      headline_details[:preview] = headline.at_css('div.post-block__content').text.strip,
       headline_details[:title]   = headline.at_css('h2.post-block__title a').text.strip
+      headline_details[:preview] = headline.at_css('div.post-block__content').text.strip
+      headline_details[:author]  = headline.at_css('div.river-byline').css('.river-byline__authors').text.strip
+      headline_details[:href]    = headline.at_css('h2.post-block__title a').attribute('href').value
 
       articles << headline_details
     end
