c4a85ac97bd8c3770e4e050b93c8dfe8396da07a
Alex Velasquez <jtinnesco@gmail.com>
2019-11-15 02:09:41 +0000

    fixed authro css selector

--- lib/tech_crunch/scraper.rb
+++ lib/tech_crunch/scraper.rb
@@ -8,10 +8,10 @@ class Scraper
     headlines = Nokogiri::HTML(html)
     headlines.css('div.post-block').each do |headline|
       headline_details = {}
-      headline_details[:title]   = headline.at_css('h2.post-block__title a').text.strip
-      headline_details[:author]  = headline.at_css('div.river-byline').css('.river-byline__authors')[0].text.strip,
+      headline_details[:author]  = headline.at_css('div.river-byline').css('.river-byline__authors').text.strip
+      headline_details[:href]    = headline.at_css('h2.post-block__title a').attribute('href').value,
       headline_details[:preview] = headline.at_css('div.post-block__content').text.strip,
-      headline_details[:href]    = headline.at_css('h2.post-block__title a').attribute('href').value
+      headline_details[:title]   = headline.at_css('h2.post-block__title a').text.strip
 
       articles << headline_details
     end
