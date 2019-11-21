1deba6ef98f84392c8a98d17270be84fa0ecee4c
Alex Velasquez <jtinnesco@gmail.com>
2019-11-21 04:03:48 +0000

    added notes

--- lib/tech_crunch/scraper.rb
+++ lib/tech_crunch/scraper.rb
@@ -1,15 +1,15 @@
 class Scraper
 
   def self.scrape_headlines(homepage_url)
-    articles = []
-    headlines = Nokogiri::HTML(open(homepage_url))
+    articles = [] # - Empty array to collect hashes below, each hash representing instance of article
+    headlines = Nokogiri::HTML(open(homepage_url)) # - Nodeset set to local variable, then iterate
     headlines.css('.post-block').each do |headline|
       headline_details = {}
       headline_details[:author]  = headline.at_css('div.river-byline').css('.river-byline__authors').text.strip
       headline_details[:href]    = headline.at_css('h2.post-block__title a').attribute('href').value
       headline_details[:preview] = headline.at_css('div.post-block__content').text.strip
       headline_details[:title]   = headline.at_css('h2.post-block__title a').text.strip
-      
+
       articles << headline_details
     end
     articles
